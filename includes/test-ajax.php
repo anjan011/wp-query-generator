317c1cbbb4e6d33a5cf2138835d8f9693f403105
Anjan Bhowmik <anjan011@gmail.com>
2015-12-12 09:41:46 +0600

    updates

--- includes/test-ajax.php
+++ includes/test-ajax.php
@@ -1,57 +1,7 @@
 <?php
 
-    // Query Args
-    $args = array(
-
-       'post_type' => array('any'),
-        'date_query' => array(
-            'relation' => 'AND',
-            array(
-                'year' => 2010,
-
-
-                'compare' => '>'
-            ),
-
-            array(
-                'year' => 2015,
-
-
-                'compare' => '<'
-            ),
-
-            /*array(
-                'year' => 2015,
-                'compare' => '<='
-            ),
-            'inclusive' => true*/
-        ),
-        'nopaging' => true,
-        'order' => 'asc',
-        'orderby' => 'date'
-    );
-
-    // The Query
-    $the_query = new WP_Query( $args );
-
-    // The Loop
-    if ( $the_query->have_posts() ) {
-
-        foreach($the_query->get_posts() as $p) {
-
-            echo formattedDate($p->post_date,'M d, Y h:i a [W]').'<br>';
-
-        }
-
-    } else {
-
-        // no posts found
-
-    }
-
-    echo '<br><br><br>';
-
-    echo $the_query->request;
-
-    // Restore original Post Data
-    wp_reset_postdata();
+    /**
+     * Ajax test playground!
+     *
+     * Assuming wordpress is installed on the root directory of the domain, the test url can be accessed here: /wp-admin/admin-ajax.php?action=meAnjanWqg_Test
+     */
